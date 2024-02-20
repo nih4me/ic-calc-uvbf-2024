497aa8ba243c97df6988c414a2e9846b649facaf
Abdoul Kader <derokabore@gmail.com>
2024-02-20 20:19:28 +0100

    Correction de bugs dans le tests pour l'addition

--- tests.py
+++ tests.py
@@ -1,10 +1,13 @@
 import unittest
-from .calculatrice import Calculatrice
+from calculatrice import Calculatrice
 
-class CalculatriceTest(unittest.TestCase):
+class TestCalculatrice(unittest.TestCase): # TestXXXX
     def test_addition(self):
         calc1 = Calculatrice(1, 2)
         self.assertEqual(calc1.addition(), 3)
 
         calc2 = Calculatrice(1.5, 2.5)
         self.assertTrue(calc2.addition() == 4)
+
+if __name__ == '__main__':
+    unittest.main()
